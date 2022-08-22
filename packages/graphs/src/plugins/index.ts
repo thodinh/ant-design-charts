e5dacb489baf009bf61c80bef68422a1ee22f55e
liufu.lf <liufu.lf@antfin.com>
2022-08-22 22:03:21 +0800

    docs: 网站更新

--- packages/graphs/src/plugins/index.ts
+++
@@ -1,5 +0,0 @@
-import { createToolbar } from './toolbar';
-import Menu from './menu';
-export type { MenuConfig } from './menu';
-
-export { createToolbar, Menu };
--- packages/graphs/tests/data/index.ts
+++
@@ -1,1 +0,0 @@
-export { RadialData } from './radial';
